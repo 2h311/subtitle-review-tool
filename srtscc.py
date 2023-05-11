ac465c98edf9b1aed7ca9be28c749c53ebf0c94c
2h311 <kcodepal@outlook.com>
2023-05-11 08:16:00 -0500

    might be the last

--- srtscc.py
+++ srtscc.py
@@ -1,4 +1,20 @@
-# print("Hello. This is a subtitle review tool")
-# import requests
+import srt
+import requests
 
-# read in an srt/scc file
+# read in our srt/scc file
+srt_file = "Uncut Gems (2019) (NetNaija.com).srt"
+with open(srt_file, "r") as fp:
+    content = fp.read()
+
+
+srt_generator = srt.parse(content)
+subtitle = next(srt_generator)
+content = subtitle.content
+
+for t in srt_generator:
+    print(t.content)
+
+
+url = "https://api.apilayer.com/bad_words?censor_character=censor_character"
+#Not working as i expected it to.
+# Got to explore other options for this
